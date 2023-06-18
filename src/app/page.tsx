62d62363b658740f379a6916d1871b485e18d295
Ellis Brown <ellis.brown@tufts.edu>
2023-06-18 15:05:51 -0400

    Add Wifi QR code website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,104 @@
-import Image from 'next/image'
+'use client';
+import React, { useState } from "react";
+import QRCode from "react-qr-code";
+import { saveAs } from 'file-saver';
+import { AiFillGithub } from 'react-icons/ai';
+import { toPng } from 'html-to-image';
 
-export default function Home() {
+
+export default function MainPage() {
+  const [ssid, setSSID] = useState("");
+  const [password, setPassword] = useState("");
+  const [encryption, setEncryption] = useState("WPA/WPA2");
+  const ref = React.useRef(null);
+
+  const generateQRCode = () => {
+    const wifiCredentials = `WIFI:S:${ssid};T:${encryption};P:${password};;`;
+
+    return wifiCredentials;
+  };
+
+  const handleDownload = () => {
+    const qrCodeValue = generateQRCode();
+    const canvas = ref?.current;
+    if (canvas === null) {
+      alert('500 server error');
+      return;
+    }
+    toPng(canvas)
+      .then(function (blob) {
+        saveAs(blob, 'wifi-qr-code.png');
+      });
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="bg-gradient-to-r from-green-500 to-blue-500 min-h-screen flex items-center justify-center flex-col">
+      <div className="bg-gray-800 p-8 rounded-lg shadow-lg text-white">
+        <h1 className="text-3xl mb-6 font-bold text-center bg-gradient-to-r from-green-500 to-blue-500 bg-clip-text text-transparent">
+          Share your Wi-Fi credentials with a QR code
+        </h1>
+        <form className="mb-4">
+          <div className="mb-2">
+            <label htmlFor="ssid" className="block text-lg font-bold">
+              SSID (Network Name):
+            </label>
+            <input
+              type="text"
+              id="ssid"
+              value={ssid}
+              onChange={(e) => setSSID(e.target.value)}
+              className="w-full bg-gray-700 text-white px-3 py-2 rounded focus:outline-none"
             />
-          </a>
+          </div>
+          <div className="mb-2">
+            <label htmlFor="password" className="block text-lg font-bold">
+              Password:
+            </label>
+            <input
+              
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full bg-gray-700 text-white px-3 py-2 rounded focus:outline-none"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="encryption" className="block text-lg font-bold">
+              Encryption:
+            </label>
+            <select
+              id="encryption"
+              value={encryption}
+              onChange={(e) => setEncryption(e.target.value)}
+              className="w-full bg-gray-700 text-white px-3 py-2 rounded focus:outline-none"
+            >
+              <option value="WPA/WPA2">WPA/WPA2</option>
+              <option value="WEP">WEP</option>
+              <option value="None">None</option>
+            </select>
+          </div>
+        </form>
+        <div className="text-center">
+          <h2 className="text-2xl mb-2 font-bold">QR Code:</h2>
+          <div className="bg-gradient-to-r from-purple-500 to-blue-500 w-60 h-60 rounded-lg mx-auto flex items-center justify-center selectMeClassName">
+            <QRCode ref={ref} value={generateQRCode()} size={220} id={'canvas'} />
+          </div>
+          <button
+            className="mt-4 bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleDownload}
+          >
+            Download QR Code
+          </button>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+      <a
+        href={'https://github.com/Ellis-Brown/contact-qr-code'}
+        className="text-gray-200 rounded-lg bg-gradient-to-r from-black to-gray-500 hover:bg-gray-600 px-4 py-2 my-4"
+      >
+        <div className="flex flex-row">
+          <AiFillGithub size={30} />&nbsp; Star on GitHub
+        </div>
+      </a>
+    </div>
+  );
+};
